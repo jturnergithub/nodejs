dac257cfeba545e56e06fe759319d4a9deea1625
Jonathan Turner <jonathan.a.turner@comcast.net>
2023-11-21 21:08:29 -0500

    TEST dependency import

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+import {degrees, radians} from "MathUtil.js"
+
 const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  res.end('Hello World!' + radians(90));
 });
 
 server.listen(PORT, () => {
